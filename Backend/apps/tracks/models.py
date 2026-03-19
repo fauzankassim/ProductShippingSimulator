9274b9f6e33b0a2af88c511c0cec07b5a2dcd8f3
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-19 15:06:18 +0800

    Create static Status model in tracks/models.py

--- Backend/apps/tracks/models.py
+++ Backend/apps/tracks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from stores.models import Transaction
 # Create your models here.
+
+class Status(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=200)
+
