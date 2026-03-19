687293e2dd121864333d052aaa7f42a7dbc53de0
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-19 15:09:18 +0800

    FIX: Change stores.models to apps.stores.models in stores/models.py

--- Backend/apps/tracks/models.py
+++ Backend/apps/tracks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from stores.models import Transaction
+from apps.stores.models import Transaction
 # Create your models here.
 
 class Status(models.Model):
