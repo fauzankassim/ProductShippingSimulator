8510be32380265cc429e399aa19a2257104e7b3b
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-19 15:07:24 +0800

    Create Track Model in tracks/models.py

--- Backend/apps/tracks/models.py
+++ Backend/apps/tracks/models.py
@@ -7,3 +7,8 @@ class Status(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=200)
 
+class Track(models.Model):
+    transaction_id = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    courier_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
