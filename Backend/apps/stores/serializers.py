c2e5bb5f26fb0d9f08c223be7fadc945a5eac9ee
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-19 14:45:55 +0800

    Add Transaction Serializers in stores/serializers.py

--- Backend/apps/stores/serializers.py
+++ Backend/apps/stores/serializers.py
@@ -1,4 +1,4 @@
-from .models import Product
+from .models import Product, Transaction
 from rest_framework import serializers
 
 class ProductSerializers(serializers.ModelSerializer):
@@ -8,3 +8,10 @@ class ProductSerializers(serializers.ModelSerializer):
         fields = ["id", "merchant", "title", "price", "created_at"]
         read_only_fields = ["merchant", "created_at"]
 
+
+class TransactionSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transaction
+        fields = ["id", "product", "customer", "created_at"]
+        read_only_fields = [ "customer", "created_at"]
