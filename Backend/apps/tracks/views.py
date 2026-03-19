e7adb88c4f82c012f498142a8c7a86ec89bde960
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-19 16:02:57 +0800

    Add TrackViewSet in the tracks/views.py

--- Backend/apps/tracks/views.py
+++ Backend/apps/tracks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Track
+from rest_framework import permissions, viewsets
+from .serializers import TrackSerializers
 
 # Create your views here.
+class TrackViewSet(viewsets.ModelViewSet):
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def perform_create(self, serializer):
+        serializer.save(courier_id=self.request.user)
