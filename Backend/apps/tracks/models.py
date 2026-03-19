f2937b0ce528e16acb17ac024c2c2a28114cb1c1
SirPojanMok <mohdfauzan0506@gmail.com>
2026-03-19 16:01:57 +0800

    Add __str__ function for Status model in tracks/models.py

--- Backend/apps/tracks/models.py
+++ Backend/apps/tracks/models.py
@@ -6,9 +6,13 @@ from apps.stores.models import Transaction
 class Status(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=200)
+    def __str__(self):
+        return self.title
+
 
 class Track(models.Model):
     transaction_id = models.ForeignKey(Transaction, on_delete=models.CASCADE)
     courier_id = models.ForeignKey(User, on_delete=models.CASCADE)
     status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
